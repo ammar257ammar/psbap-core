a403ddb2844b8bcac73ab5e94261e30fe7b0aca3
Ammar Ammar <ammar257ammar@gmail.com>
2019-12-07 23:40:47 +0100

    Add method to load data from PdbBind two files download from main website

--- src/main/java/nl/unimaas/msb/psbap/model/PdbBindDataset.java
+++ src/main/java/nl/unimaas/msb/psbap/model/PdbBindDataset.java
@@ -20,8 +20,13 @@
 
 package nl.unimaas.msb.psbap.model;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import nl.unimaas.msb.psbap.model.PdbBindDataset;
 import nl.unimaas.msb.psbap.Config;
@@ -58,6 +63,7 @@ public class PdbBindDataset {
 	
 	/**
 	 * Create a new PdbBindDataset instance
+	 * 
 	 * @return a new PdbBindDataset with initialized variables
 	 */
 	public static PdbBindDataset create() {
@@ -66,6 +72,7 @@ public class PdbBindDataset {
 	
 	/**
 	 * Create a new PdbBindDataset instance with arguments provided
+	 * 
 	 * @param pathGeneralFile from the PdbBind downloaded file provides (PDB, Ligand, resolution) information 
 	 * @param pathGeneralNamesFile from the PdbBind downloaded file provides Uniprot IDs
 	 * @param entriesPath path of the PdbBind entries folders as extracted from the dataset downloaded file
@@ -73,6 +80,75 @@ public class PdbBindDataset {
 	 */
 	public static PdbBindDataset create(String pathGeneralFile, String pathGeneralNamesFile, String entriesPath) {
 	    return new PdbBindDataset(pathGeneralFile, pathGeneralNamesFile, entriesPath);
+	}
+	
+	/**
+	 * Load the data from the two files provided by PdbBind downloaded file <http://www.pdbbind.org.cn>
+	 * @return the current PdbBindDataset after loading "pdbbindData" List<String> with data
+	 * from both files of PdbBind
+	 */
+	public PdbBindDataset loadData() {
+		
+		List<String[]> generalFileList = new ArrayList<String[]>(); // to hold the data from first file
+		List<String[]> generalNamesFileList = new ArrayList<String[]>(); // to hold the data from second file
+		
+		try (Stream<String> stream = Files.lines(Paths.get(this.pathGeneralFile))) {
+			
+			generalFileList = stream.skip(6). // the first six lines are header, so skipped
+					map(line -> { // map each line to a function that returns a String array with 3 values
+						
+						String[] lineArr = new String[3];
+						
+						// ligand ID is wrapped inside parenthesis so we need to extract it
+						// by using substring between the index of the open and close parenthesis 
+						int ligandStart = line.indexOf("(")+1;
+						int ligandEnd = line.indexOf(")");
+						
+						lineArr[0] = line.substring(0,4); 					// PDB
+						lineArr[1] = line.substring(6,10); 					// Resolution
+						lineArr[2] = line.substring(ligandStart,ligandEnd); // Ligand
+						
+						return  lineArr;
+					}).
+					collect(Collectors.toList()); // return the results as List<String>
+	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try (Stream<String> stream = Files.lines(Paths.get(this.pathGeneralNamesFile))) {
+			
+			generalNamesFileList = stream.skip(6). // the first six lines are header, so skipped
+					map(line -> { // map each line to a function that returns a String array with 2 values
+						
+						String[] lineArr = new String[2];
+						
+						lineArr[0] = line.substring(0,4);   // PDB
+						lineArr[1] = line.substring(12,18); // Uniprot
+						
+						return  lineArr;
+					}).
+					collect(Collectors.toList()); // return the results as List<String>
+	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Left join the Uniprot IDs list (2nd list) to the first one on PDB ID 
+		// to merge the two lists in one
+		for(String[] general : generalFileList) {
+			for(String[] generalNames : generalNamesFileList) {
+				
+				// if the PDB ID matches, add a new row to the main List containing data from both lists 
+				if(general[0].equals(generalNames[0])) {
+					
+					this.pdbbindData.add(new String[]{general[0], general[1], general[2], generalNames[1]});
+					break;
+				}
+			}
+		}
+			
+		return this;
 	}
 	
 
