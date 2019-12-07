c4aa42608512371d1d954b771bd9f5cae4345c71
Ammar Ammar <ammar257ammar@gmail.com>
2019-12-07 23:47:52 +0100

    Create a utility class to manipulate List<String> datasets

---
+++ src/main/java/nl/unimaas/msb/psbap/utils/DataHandler.java
@@ -0,0 +1,33 @@
+/**
+* binding Pocket's SNPS effect on Binding Affinity Project (PSBAP) 
+* 
+*Copyright (C) 2019  Ammar Ammar <ammar257ammar@gmail.com>
+*
+*This program is free software: you can redistribute it and/or modify
+*it under the terms of the GNU Affero General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*This program is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU Affero General Public License for more details.
+*
+*You should have received a copy of the GNU Affero General Public License
+*along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+package nl.unimaas.msb.psbap.utils;
+
+/**
+ * 
+ * @author Ammar Ammar
+ * 
+ * A utility class to perform operations on datasets of type List<String[]>
+ * It can print first rows of the dataset, return number of rows and columns, 
+ * and write the dataset to TSV file
+ */
+public class DataHandler {
+
+}
