3b069d3bb5f8b11009eb2ae62b0fa93f1bf47f73
Ammar Ammar <ammar257ammar@gmail.com>
2019-12-07 22:12:36 +0100

    document the Config class

--- src/main/java/nl/unimaas/msb/psbap/Config.java
+++ src/main/java/nl/unimaas/msb/psbap/Config.java
@@ -1,3 +1,23 @@
+/**
+* binding Pocket's SNPS effect on Binding Affinity Project (PSBAP) 
+* 
+*Copyright (C) 2019  Ammar Ammar <ammar257ammar@gmail.com>
+*
+*This program is free software: you can redistribute it and/or modify
+*it under the terms of the GNU Affero General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*This program is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU Affero General Public License for more details.
+*
+*You should have received a copy of the GNU Affero General Public License
+*along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
 package nl.unimaas.msb.psbap;
 
 import java.io.FileInputStream;
@@ -5,6 +25,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * 
+ * @author Ammar Ammar
+ * 
+ * Base class to read the configuration from a config file 
+ * and load it into a Properties object
+ * 
+ * The class assumes a file named "config.properties" next to 
+ * the application JAR and tries to load it
+ *
+ */
 public class Config {
 	
 	private static Properties properties = new Properties();
@@ -34,5 +65,14 @@ public class Config {
 		}
 	}
 
+	/**
+	 * Get the property value of the requested key
+	 * 
+	 * @param key is a string which you want to retrieve the value for
+	 * @return a string holding the value of the requested key
+	 */
+	public static String getProperty(String key) {
+		return properties.getProperty(key);
+	}
 
 }
