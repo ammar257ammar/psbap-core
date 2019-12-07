5d8a24f316f2afc4531b6d701391959b7a47f568
Ammar Ammar <ammar257ammar@gmail.com>
2019-12-07 21:04:37 +0100

    Read config file into Properties object

--- src/main/java/nl/unimaas/msb/psbap/Config.java
+++ src/main/java/nl/unimaas/msb/psbap/Config.java
@@ -1,5 +1,38 @@
 package nl.unimaas.msb.psbap;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Config {
+	
+	private static Properties properties = new Properties();
+	private static InputStream input = null;
+
+	static {
+
+		try {
+
+			input = new FileInputStream("config.properties");
+
+			properties.load(input);
+
+		} catch (IOException ex) {
+
+			ex.printStackTrace();
+
+		} finally {
+
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 
 }
