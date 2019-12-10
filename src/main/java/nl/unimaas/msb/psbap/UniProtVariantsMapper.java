061cba2a1fd1665a723d2e892e02711989c92f0f
Ammar Ammar <ammar257ammar@gmail.com>
2019-12-10 02:52:51 +0100

    create a method to filter missense variants that occur in PdbBind proteins

--- src/main/java/nl/unimaas/msb/psbap/UniProtVariantsMapper.java
+++ src/main/java/nl/unimaas/msb/psbap/UniProtVariantsMapper.java
@@ -42,7 +42,53 @@ import nl.unimaas.msb.psbap.model.PdbBindDataset.PdbbindAttribute;
  */
 public class UniProtVariantsMapper {
 	
+	/**
+	 * A method to filter missense variants that occur in PdbBind proteins
+	 * @param pdbbindDataset
+	 * @return a dataset with all missence variants in PdbBind proteins
+	 */
+	public static List<String[]> mapMissenseVariantsToPdbbindDataset(List<String[]> pdbbindDataset) {
+		
+		List<String[]> pdbbindVariantsRows = new ArrayList<String[]>();
+		
+		try {
+			LineIterator it = FileUtils.lineIterator(new File(Config.getProperty("UNIPROT_VARIANTS_PATH")), "UTF-8");
+		
+			try {
+				
+			    while (it.hasNext()) {
+			    				    	
+			    	String line = it.nextLine();
 
+			    	String[] lineArr = line.split("\t");
+
+			        if(lineArr.length > 5 && lineArr[4].toLowerCase().contains("missense")) {
+
+			        	for(String[] row: pdbbindDataset) {
+
+			        		if(lineArr[1].trim().toLowerCase().equals(row[PdbbindAttribute.UNIPROT.ordinal()].trim().toLowerCase())) {
+			        			
+			        			pdbbindVariantsRows.add(new String[] {lineArr[1], 
+					        			lineArr[2], 
+					        			lineArr[3],
+					        			lineArr[4],
+					        			row[PdbbindAttribute.PDB.ordinal()],
+					        			row[PdbbindAttribute.RESOLUTION.ordinal()],
+					        			row[PdbbindAttribute.LIGAND.ordinal()]
+					        			});
+			        			break;
+			        		}
+			        	}
+			        }	    
+			    }
+				
+			} finally {
+				it.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return pdbbindVariantsRows;
+	}
 	
-
 }
