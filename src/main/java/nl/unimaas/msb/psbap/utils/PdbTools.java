67066509f43fb2619a9600b90006b8056c4b4e76
Ammar Ammar <ammar257ammar@gmail.com>
2019-12-11 00:18:04 +0100

    organize imports

--- src/main/java/nl/unimaas/msb/psbap/utils/PdbTools.java
+++ src/main/java/nl/unimaas/msb/psbap/utils/PdbTools.java
@@ -27,11 +27,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import org.biojava.nbio.core.util.InputStreamProvider;
 import org.biojava.nbio.structure.AminoAcid;
