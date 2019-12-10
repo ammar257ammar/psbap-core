d04f4c70df558e0fbf46472a1c4aba6553812b51
Ammar Ammar <ammar257ammar@gmail.com>
2019-12-10 02:50:46 +0100

    Create a class to map between Uniprot variants and PdbBind proteins

---
+++ src/main/java/nl/unimaas/msb/psbap/UniProtVariantsMapper.java
@@ -0,0 +1,48 @@
+
+/**
+* binding Pocket's SNPs effect on Binding Affinity Project (PSBAP) 
+* 
+*Copyright (C) 2019  Ammar Ammar <ammar257ammar@gmail.com>
+*
+*This program is free software: you can redistribute it and/or modify
+*it under the terms of the GNU Affero General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*This program is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU Affero General Public License for more details.
+*
+*You should have received a copy of the GNU Affero General Public License
+*along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+package nl.unimaas.msb.psbap;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
+import nl.unimaas.msb.psbap.Config;
+import nl.unimaas.msb.psbap.model.PdbBindDataset.PdbbindAttribute;
+
+
+/**
+ * @author Ammar Ammar
+ * 
+ * This class map between the Uniprot variants dataset with PdbBind dataset and filter the 
+ * missence variants that occur in PdbBind proteins only
+ * 
+ */
+public class UniProtVariantsMapper {
+	
+
+	
+
+}
