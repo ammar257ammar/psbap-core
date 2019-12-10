ce93b199f9d18471e6afc3d78f6dfb4812a91f19
Ammar Ammar <ammar257ammar@gmail.com>
2019-12-10 21:39:06 +0100

    Add two methods to generate the PdbBind dataset and write it to TSV file

--- src/main/java/nl/unimaas/msb/psbap/Tasks.java
+++ src/main/java/nl/unimaas/msb/psbap/Tasks.java
@@ -20,6 +20,10 @@
 
 package nl.unimaas.msb.psbap;
 
+import nl.unimaas.msb.psbap.model.PdbBindDataset;
+import nl.unimaas.msb.psbap.model.PdbBindDataset.PdbbindAttribute;
+import nl.unimaas.msb.psbap.utils.DataHandler;
+
 /**
  * A class with static methods to perform the steps in the PSBAP project via calling them from command line
  * 
@@ -28,5 +32,35 @@ package nl.unimaas.msb.psbap;
  */
 public class Tasks {
 	
+	/**
+	 * This is usually the first step in the workflow. 
+	 * It generates PdbBind dataset filtered, sorted and grouped by UniProt ID.
+	 * It uses a threshold of 2.51 Angstrom and remove PDBs obtained with NMR and PDBs withou UniProt ID
+	 * 
+	 * @return PdbBindDataset which is the main block for the project
+	 */
+	public static PdbBindDataset generatePdbBindDataset(){
+		
+		PdbBindDataset pdbbindData = PdbBindDataset.create().
+											loadData().
+											filterStringNotEqual(PdbbindAttribute.UNIPROT, "------").
+											filterStringNotEqual(PdbbindAttribute.RESOLUTION, "NMR").
+											sortBy(PdbbindAttribute.RESOLUTION).
+											keepAsFolderMatch().
+											filterDoubleCutoff(PdbbindAttribute.RESOLUTION, 2.51).
+											groupByUniProtAndKeepMinResolution(true);
+		
+		return pdbbindData;		
+	}
 	
+	/**
+	 * A method to write PdbBindDataset list to a TSV file
+	 * @param pdbbindData which is the generated PdbBindDataset object
+	 * @param path which is the path of TSV file
+	 */
+	public static void writePdbBindDatasetToFile(PdbBindDataset pdbbindData, String path){
+		
+    	DataHandler.writeDatasetToTSV(pdbbindData.getData(), path + "/pdbbind_entries_data.tsv");
+	}
+
 }
