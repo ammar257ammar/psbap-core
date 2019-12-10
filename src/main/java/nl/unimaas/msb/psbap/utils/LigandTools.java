03bb71df30e16f34be22ebcc4d71f5756803ce9a
Ammar Ammar <ammar257ammar@gmail.com>
2019-12-10 22:20:57 +0100

    Create a method to parse PdbBind entry ligand file to IAtmoContainer

--- src/main/java/nl/unimaas/msb/psbap/utils/LigandTools.java
+++ src/main/java/nl/unimaas/msb/psbap/utils/LigandTools.java
@@ -20,6 +20,24 @@
 
 package nl.unimaas.msb.psbap.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.openscience.cdk.ChemFile;
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.aromaticity.Aromaticity;
+import org.openscience.cdk.aromaticity.ElectronDonation;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.graph.Cycles;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomContainerSet;
+import org.openscience.cdk.interfaces.IChemModel;
+import org.openscience.cdk.interfaces.IChemSequence;
+import org.openscience.cdk.io.MDLV2000Reader;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
+
 /**
  * A utility class to manipulate ligand files mainly using CDK (Mol2 and SDF formats)
  * 
@@ -28,4 +46,49 @@ package nl.unimaas.msb.psbap.utils;
  */
 public class LigandTools {
 
+
+	/**
+	 * A method to read a molecule file from PdbBind entry folder and parse it
+	 * as a CDK IAtomContainer
+	 * @param fname the molecule file path
+	 * @return
+	 */
+	public static IAtomContainer readMolandAromatizeKekulize(String fname){
+		
+		String filename = fname;
+		
+		try{
+		
+			InputStream ins = new FileInputStream(new File(filename));
+	        
+			MDLV2000Reader reader = new MDLV2000Reader(ins);
+	        
+	        ChemFile chemFile = (ChemFile) reader.read((ChemObject) new ChemFile());
+	        
+	        IChemSequence cs = chemFile.getChemSequence(0);
+	                
+	        IChemModel cm = cs.getChemModel(0);
+	        
+	        IAtomContainerSet acs = cm.getMoleculeSet();
+	                
+	        reader.close();
+	        
+	        IAtomContainer ac = acs.getAtomContainer(0);
+	        
+	        AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(ac);
+	        
+	        ElectronDonation ed = ElectronDonation.daylight();
+			
+			Aromaticity ar = new Aromaticity(ed, Cycles.all());
+			
+			ar.apply(ac);
+				                        
+	        return ac;  
+	        
+		} catch (CDKException e) {
+			return null;
+		} catch (IOException e) {
+			return null;
+		}
+	}
 }
