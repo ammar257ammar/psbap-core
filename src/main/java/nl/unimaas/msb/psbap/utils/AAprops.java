8fd2eab3c8dc4b240efd76f8cf4061803db19968
apc <you@example.com>
2020-05-07 04:53:33 +0200

    add a method to calculate the properties of a specific residue AA and its surrounding environment

--- src/main/java/nl/unimaas/msb/psbap/utils/AAprops.java
+++ src/main/java/nl/unimaas/msb/psbap/utils/AAprops.java
@@ -20,11 +20,16 @@
 
 package nl.unimaas.msb.psbap.utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.biojava.nbio.structure.AminoAcid;
 import org.biojava.nbio.structure.Calc;
+import org.biojava.nbio.structure.Structure;
+import org.biojava.nbio.structure.io.PDBFileReader;
 
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
@@ -86,6 +91,99 @@ public class AAprops {
 		}
 
 		return neighbours;
+	}
+	
+
+	/**
+	 * A method to calculate the properties of a specific residue AA and its surrounding environment
+	 * @param path a path string for the PDB of the protein
+	 * @param residueNumber the targeted residue number
+	 * @param WT the wild-type residue name
+	 * @return list of 96 double values (48 for residue properties, 48 for surrounding properties)
+	 */
+	public static List<Double> getResidueAndSurroundingProps(String path, String residueNumber, String WT) throws IOException{
+		
+		List<Double> props = new ArrayList<Double>();
+		List<Double> nbProps = new ArrayList<Double>();
+		
+		HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+		map.put('A', 1);
+		map.put('R', 15);
+		map.put('N', 12);
+		map.put('D', 2);
+		map.put('C', 3);
+		map.put('Q', 14);
+		map.put('E', 4);
+		map.put('G', 6);
+		map.put('H', 7);
+		map.put('I', 8);
+		map.put('L', 10);
+		map.put('K', 9);
+		map.put('M', 11);
+		map.put('F', 5);
+		map.put('P', 13);
+		map.put('S', 16);
+		map.put('T', 17);
+		map.put('W', 19);
+		map.put('Y', 20);
+		map.put('V', 18);
+		
+		PDBFileReader reader = PdbTools.configureReader(false);
+
+		Structure proteinStructure = reader.getStructure(path);
+
+		List<AminoAcid> aminoAcids = PdbTools.getAminoAcidsFromStructure(proteinStructure);
+		
+		for (int i = 0; i < aminoAcids.size(); i++) {
+
+			if (aminoAcids.get(i).getResidueNumber().toString().equals(residueNumber)) {
+				
+				AminoAcid residue = aminoAcids.get(i);
+				
+				CsvParserSettings settings = new CsvParserSettings();
+
+				settings.getFormat().setLineSeparator("\n");
+				settings.getFormat().setDelimiter(',');
+
+				settings.setNumberOfRowsToSkip(1);
+				
+				CsvParser parser = new CsvParser(settings);
+
+				List<String[]> rows = parser.parseAll(new File("config/AAprops.csv"));
+
+				List<AminoAcid> neighbours = AAprops.getResidueNeighbours(aminoAcids, residue);
+
+				for(String[] row: rows) {
+
+					double aaPropValue = Double.parseDouble(row[map.get(residue.getAminoType())]);
+					double aaWTPropValue = Double.parseDouble(row[map.get(WT.trim().charAt(0))]);
+
+					double neighboursPropValue = 0.0;
+
+					props.add(aaPropValue-aaWTPropValue);
+					
+					if(neighbours.size() > 0) {
+						
+						for(AminoAcid nb: neighbours) {
+							neighboursPropValue += Double.parseDouble(row[map.get(nb.getAminoType())]);
+						}
+						
+						nbProps.add(neighboursPropValue - aaPropValue);
+
+					}else {
+						nbProps.add(0.0);
+					}
+					
+				}
+				
+				props.addAll(nbProps);
+				
+				break;
+
+			}
+		}
+		
+		return props;
 	}
 
 }
