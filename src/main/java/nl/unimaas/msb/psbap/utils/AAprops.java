539defe8941a7725e5f449411c7067727303cf7e
Ammar Ammar <ammar257ammar@gmail.com>
2020-05-07 04:46:41 +0200

    create a utility class to handle AA properties

---
+++ src/main/java/nl/unimaas/msb/psbap/utils/AAprops.java
@@ -0,0 +1,36 @@
+/**
+* binding Pocket's SNPs effect on Binding Affinity Project (PSBAP) 
+* 
+*Copyright (C) 2019  Ammar Ammar <ammar257ammar@gmail.com>
+*
+*This program is free software: you can redistribute it and/or modify
+*it under the terms of the GNU Affero General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*This program is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU Affero General Public License for more details.
+*
+*You should have received a copy of the GNU Affero General Public License
+*along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+package nl.unimaas.msb.psbap.utils;
+
+
+/**
+ * A utility class to use 48 amino acid properties in creating features for AA residues and their surrounding
+ * It can calculate the AA properties for all residues surrounding a specific residue with 8A radius
+ * and return the results as a list of double values
+ * 
+ * @author Ammar Ammar
+ * 
+ */
+public class AAprops {
+		
+	
+
+}
