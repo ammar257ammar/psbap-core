1d3f99d34e4685fca46d1053a50c603d7c642233
apc <you@example.com>
2020-05-26 00:08:33 +0200

    add class to prepare ligands data and folder structure

---
+++ src/main/java/nl/unimaas/msb/psbap/Ligand3D.java
@@ -0,0 +1,34 @@
+/**
+* binding Pocket's SNPs effect on Binding Affinity Project (PSBAP) 
+* 
+*Copyright (C) 2019  Ammar Ammar <ammar257ammar@gmail.com>
+*
+*This program is free software: you can redistribute it and/or modify
+*it under the terms of the GNU Affero General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*This program is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU Affero General Public License for more details.
+*
+*You should have received a copy of the GNU Affero General Public License
+*along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+package nl.unimaas.msb.psbap;
+
+
+/**
+ * A class to prepare ligands folder structure and prepare a dataset of ligands ChEMBL IDs and Tanimoto similarities
+ * 
+ * @author Ammar Ammar
+ *
+ */
+public class Ligand3D {
+	
+	
+
+}
