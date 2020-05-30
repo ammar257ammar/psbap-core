244d18f0ddda6e4488007042a601988eb90e5b49
Ammar Ammar <ammar257ammar@gmail.com>
2020-05-30 12:45:01 +0200

    Add a method to generate Vina folder from Gromacs results for one PDB

--- src/main/java/nl/unimaas/msb/psbap/Vina.java
+++ src/main/java/nl/unimaas/msb/psbap/Vina.java
@@ -20,6 +20,11 @@
 
 package nl.unimaas.msb.psbap;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 /**
  * A class to prepare AutoDock Vina folder structure and prepare the grid box and extract binding affinities from docking results
  * 
@@ -27,5 +32,51 @@ package nl.unimaas.msb.psbap;
  *
  */
 public class Vina {
+	
+	
+	/**
+	 * A method to generate folder structure for AutoDock Vina from Gromacs EM results folders for one PDB bind entry
+	 * @param gromacsPath the path for Gromacs folder where EM took place
+	 * @param pdb the PdbBind entry
+	 * @param vinaPath the AutoDock Vina folder being prepared docking
+	 * @throws IOException in case of error in IO operations
+	 */
+	public static void createFolderStructureFromGromacsOneFolder(String gromacsPath, String pdb, String vinaPath) throws IOException {
+
+		int index = 1;
+
+		File molVarsFolder = new File(gromacsPath+pdb+"/output");
+		File[] molVars = molVarsFolder.listFiles();
+		
+		File newMolFolder = new File(vinaPath+pdb);
+		
+		if(!newMolFolder.exists()) {
+			newMolFolder.mkdir();
+		}
+		
+		for(File molVarFolder: molVars) {
+			
+			if(molVarFolder.isDirectory()) {
+				
+				File newMolVarFolder = new File(vinaPath+pdb+"/proteins/"+molVarFolder.getName());
+
+				if(!newMolVarFolder.exists()) {
+					newMolVarFolder.mkdir();
+				}
+
+				String oldMolVarFolder = gromacsPath+pdb+"/output/"+molVarFolder.getName();
+				String newMolVarFolderStr = vinaPath+pdb+"/proteins/"+molVarFolder.getName();
+
+				File src1 = new File(oldMolVarFolder+"/"+molVarFolder.getName()+"_final.pdb");
+				File dst1 = new File(newMolVarFolderStr+"/"+molVarFolder.getName()+"_final.pdb");
+				
+				FileUtils.copyFile(src1, dst1);	
+				
+				System.out.println("Varinat folder num: "+index);
+				index++;
+				
+			}
+		}	
+	}
 
 }
